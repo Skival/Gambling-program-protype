a6effb46cdcf61e1fdbbc5f6cd23ad3dcad8bd57
Skival <107822537+Skival@users.noreply.github.com>
2025-11-12 15:32:19 -0600

    Add files via upload
    This is the 1st prototype that is slightly rigged at 3x, but otherwise is fine.

---
+++ temp.py
@@ -0,0 +1,107 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+print('hello world');
+n = int(input("Enter a number (hint: 1 is better) "));
+for i in range(n):
+#i'll change to while loop later
+    print("Ok bud heres the deal");
+    print("The time is now to gamble");
+    totalbalance = int(input("How much money is in your wallet?"))
+    balance = int(input("How much money do you want to put in?"));
+    #these if balance things were just for trolling ngl
+    if balance < n;:
+        print("I'm gonna slime you...");
+    if balance > n;:
+    multiplier = (int(input("What would you like to multiply it by? chances decrease with every number Ex: 2 = 50% 3 = 33.3% 4 = 25% 5 = 20% (5 is the max)")));
+    if multiplier == (2):
+        one == randint(1,100) and two == randint(50,100)
+            if two >= one:
+                print("Congratulations, Your money has been doubled!")
+                totalbalance = totalbalance + balance
+                print("Here is what your finances look like")
+                print("Whats in your wallet: " + totalbalance)
+                print("What you have gained: " + (balance*2))
+                Ccontinue = input("Continue? (y/n)")
+                if Ccontinue == y :
+                    continue
+                else return;
+            else 
+                print("You have lost.")
+            totalbalance = totalbalance - balance
+                print("Here is what your finances look like")
+                print("Whats in your wallet: " + totalbalance)
+                print("What you have lost: " + (balance))
+                Ccontinue = input("Continue? (y/n)")
+                if Ccontinue == y :
+                    continue
+                else return;
+    if multiplier == (3):
+        one == randint(1,100) and two == randint(1,33)
+            if two <= one:
+                print("Congratulations, Your money has been tripled!")
+                totalbalance = totalbalance + balance
+                print("Here is what your finances look like")
+                print("Whats in your wallet: " + totalbalance)
+                print("What you have gained: " + (balance*3))
+                Ccontinue = input("Continue? (y/n)")
+                if Ccontinue == y :
+                    continue
+                else return;
+            else 
+                print("You have lost.")
+            totalbalance = totalbalance - balance
+                print("Here is what your finances look like")
+                print("Whats in your wallet: " + totalbalance)
+                print("What you have lost: " + (balance))
+                Ccontinue = input("Continue? (y/n)")
+                if Ccontinue == y :
+                    continue
+                else return;
+    if multiplier == (4):
+        one == randint(1,100) and two == randint(1,25)
+            if two <= one:
+                print("Congratulations, Your money has been quadoupled!")
+                totalbalance = totalbalance + balance
+                print("Here is what your finances look like")
+                print("Whats in your wallet: " + totalbalance)
+                print("What you have gained: " + (balance*4))
+                Ccontinue = input("Continue? (y/n)")
+                if Ccontinue == y :
+                    continue
+                else return;
+            else 
+                print("You have lost.")
+            totalbalance = totalbalance - balance
+                print("Here is what your finances look like")
+                print("Whats in your wallet: " + totalbalance)
+                print("What you have lost: " + (balance))
+                Ccontinue = input("Continue? (y/n)")
+                if Ccontinue == y :
+                    continue
+                else return;
+    if multiplier == (5):
+        one == randint(1,100) and two == randint(1,20)
+            if two <= one:
+                print("Congratulations, Your money has been Quintupled!")
+                totalbalance = totalbalance + balance
+                print("Here is what your finances look like")
+                print("Whats in your wallet: " + totalbalance)
+                print("What you have gained: " + (balance*5))
+                Ccontinue = input("Continue? (y/n)")
+                if Ccontinue == y :
+                    continue
+                else return;
+            else 
+                print("You have lost.")
+            totalbalance = totalbalance - balance
+                print("Here is what your finances look like")
+                print("Whats in your wallet: " + totalbalance)
+                print("What you have lost: " + (balance))
+                Ccontinue = input("Continue? (y/n)")
+                if Ccontinue == y :
+                    continue
+                else return;
